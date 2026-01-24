6df4859584f1e40e6935bfbb4eb9026a1063dd71
Yap Jun Bin Daniel <danielyap4103@gmail.com>
2026-01-24 23:21:58 +0800

    Add ability to store items and list them

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Atlas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] items = new String[100];
+        int count = 0;
 
         System.out.println("Hello! I'm Atlas");
         System.out.println("What can I do for you?");
@@ -13,9 +15,15 @@ public class Atlas {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + "." + items[i]);
+                }
+            } else {
+                System.out.println("added: " + input);
+                items[count] = input;
+                count++;
             }
-
-            System.out.println(input);
         }
 
         scanner.close();
