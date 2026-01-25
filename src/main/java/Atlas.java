3b2cd5afd498aa548ce5c36f6c94ae7f73c1986e
Yap Jun Bin Daniel <danielyap4103@gmail.com>
2026-01-25 18:06:53 +0800

    Added functionality to deal with incorrect inputs

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -10,46 +10,69 @@ public class Atlas {
         System.out.println("What can I do for you?");
 
         while (true) {
-            String input = scanner.nextLine();
+            try {
+                String input = scanner.nextLine();
 
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + "." + tasks[i]);
+                if (input.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                } else if (input.equals("list")) {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < count; i++) {
+                        System.out.println((i + 1) + "." + tasks[i]);
+                    }
+                } else if (input.startsWith("mark")) {
+                    if (input.length() <= 4) {
+                        throw new AtlasException("mark has to be used with the task index following it");
+                    }
+                    int index = Integer.parseInt(input.substring(5)) - 1;
+                    tasks[index].markDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[index]);
+                } else if (input.startsWith("unmark")) {
+                    if (input.length() <= 6) {
+                        throw new AtlasException("unmark has to be used with the task index following it");
+                    }
+                    int index = Integer.parseInt(input.substring(7)) - 1;
+                    tasks[index].unmark();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[index]);
+                } else if (input.startsWith("todo")) {
+                    if (input.length() <= 4) {
+                        throw new AtlasException("The description of a todo cannot be empty.");
+                    }
+                    String taskName = input.substring(5);
+                    tasks[count++] = new Todo(taskName);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[count - 1]);
+                    System.out.println("Now you have " + count + " tasks in the list.");
+                } else if (input.startsWith("deadline")) {
+                    String[] parts = input.substring(9).split(" /by ");
+                    if (parts.length < 2) {
+                        throw new AtlasException("Deadline must have a /by date.");
+                    }
+                    tasks[count++] = new Deadline(parts[0], parts[1]);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[count - 1]);
+                    System.out.println("Now you have " + count + " tasks in the list.");
+                } else if (input.startsWith("event")) {
+                    String[] parts = input.substring(6).split(" /from | /to ");
+                    if (parts.length < 3) {
+                        throw new AtlasException("Event must have /from and /to.");
+                    }
+                    tasks[count++] = new Event(parts[0], parts[1], parts[2]);
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + tasks[count - 1]);
+                    System.out.println("Now you have " + count + " tasks in the list.");
+                } else {
+                    throw new AtlasException("No such command exists");
                 }
-            } else if (input.startsWith("mark ")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
-                tasks[index].markDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + tasks[index]);
-            } else if (input.startsWith("unmark ")) {
-                int index = Integer.parseInt(input.substring(7)) - 1;
-                tasks[index].unmark();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + tasks[index]);
-            } else if (input.startsWith("todo ")) {
-                String taskName = input.substring(5);
-                tasks[count++] = new Todo(taskName);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[count - 1]);
-                System.out.println("Now you have " + count + " tasks in the list.");
-            } else if (input.startsWith("deadline ")) {
-                String[] parts = input.substring(9).split(" /by ");
-                tasks[count++] = new Deadline(parts[0], parts[1]);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[count - 1]);
-                System.out.println("Now you have " + count + " tasks in the list.");
-            } else if (input.startsWith("event ")) {
-                String[] parts = input.substring(6).split(" /from | /to ");
-                tasks[count++] = new Event(parts[0], parts[1], parts[2]);
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + tasks[count - 1]);
-                System.out.println("Now you have " + count + " tasks in the list.");
+
+            } catch (AtlasException e) {
+                System.out.println("OOPS!!! " + e.getMessage());
             }
         }
+
 
         scanner.close();
     }
